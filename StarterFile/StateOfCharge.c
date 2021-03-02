744ce17ffaa362e61ae776c2a0b276c10bc633bb
shinleon <shinleon@uw.edu>
2021-03-01 23:44:10 -0800

    made the bi-lin interp for soc

--- StarterFile/StateOfCharge.c
+++ StarterFile/StateOfCharge.c
@@ -2,18 +2,93 @@
 #include <stdbool.h>
 #include "StateOfCharge.h"
 
-/***********************************************************************
+
+
+float computeSoc(const float* volt, const float* current, const float* temp)
+{
+    float resistance = 0.5;
+    float open_cVolt = volt + resistance * current; 
+    float left_v = -1; //hold the index of the largest v less than volt
+    float right_v = -1; //hold the index of the smallest v larger than volt
+    float lower_temp = -1; //hold the index of the smallest t larger than temp
+    float upper_temp = -1; //hold the index of the largest t smaller than temp
+
+    for(int i = 0; i < volt_length; i++)
+    {
+        if(open_cVolt < voltages_soc[i])
+        {
+            if (i == 0)
+            {
+                return 0;
+            }
+            else if(right_v == -1)
+            {
+                right_v = i;
+            }
+        }
+        if(open_cVolt > voltages_soc[i])
+        {
+            if (i == volt_length - 1)
+            {
+                return 100;
+            }
+            else if(left_v == -1)
+            {
+                left_v = i;
+            }
+        }
+    }
+
+    for(int i = 0; i < temp_length; i++)
+    {
+        if(temp < temperatures_soc[i])
+        {
+            if (i == 0)
+            {
+                return -1; //temp is below -10
+            }
+            else if(right_v == -1)
+            {
+                upper_temp = i;
+            }
+        }
+        if(temp > temperatures_soc[i])
+        {
+            if (i == volt_length - 1)
+            {
+                return -1;   //temp is above 45
+            }
+            else if(left_v == -1)
+            {
+                lower_temp = i;
+            }
+        }
+    }
+    //interpolation at upper temp w respect to voltage
+    float voltage_ratio = (open_cVolt - voltages_soc[left_v])/(voltages_soc[right_v]-voltages_soc[left_v]);
+    float inter_upper = voltage_ratio *
+                        (percent_soc[lower_temp][right_v] - percent_soc[lower_temp][left_v])
+                         + percent_soc[lower_temp][left_v];
+    //interpolation at lower temp w respect to voltage
+    float inter_lower = voltage_ratio *
+                        (percent_soc[upper_temp][right_v] - percent_soc[upper_temp][left_v]) 
+                         + percent_soc[upper_temp][left_v];
+    
+    //interpolate with respect to temperature
+    float temp_ratio = (temp - temperatur_soc[lower_temp])/(temperatures_soc[upper_temp] - temperatures_soc[lower_temp]);
+    float soc = inter_upper - temp_ratio*(inter_upper - inter_lower);
+    return soc;
+}
+/************************************************************************
   * Function name: stateOfChargeTask
   * Function inputs: generic pointer to the updated state of charge data
   * Function outputs: ~
-  * Function description: This function calls an update to in order to
-  *                       update the state of charge. It cycles through
-  *                       the values 0, 50, and 100 at a 1 second rate.
+  * Function description: update state of charge using bi-linear interpolation
   * Author(s): Leonard Shin, Leika Yamada
   **********************************************************************/
 void stateOfChargeTask ( void* socData ) {
   
-    // Purposefully left empty
-    
+    stateOfChargeData* data = (stateOfChargeData*) socData;
+    data->soc = computeSoc(data->vTerminal, data->iTerminal, data->temp);
   return;
 }
--- StarterFile/StateOfCharge.h
+++ StarterFile/StateOfCharge.h
@@ -11,9 +11,23 @@ extern "C" {
 #include <Arduino.h>
 
 
+int volt_length = 5;
+int voltages_soc[] = {200, 250, 300, 350, 400};   // columns
+int temp_length = 4;
+int temperatures_soc[] = {-10, 0, 25, 45};        // rows
+int percent_soc[][] = {
+                   {  0,  0,  35, 100, 100},
+                   {  0,  0,  20,  80, 100},
+                   {  0,  0,  10,  60, 100},
+                   {  0,  0,   0,  50, 100}
+                  };
+
 typedef struct stateOfChargeTaskData {      // Defines a data struct to hold the SOC
   
-    // purposefully left blank
+    float* vTerminal;
+    float* iTerminal;
+    float* temp;
+    float* soc;
     
 } stateOfChargeData;
 
